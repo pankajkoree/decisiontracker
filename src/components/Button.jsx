71a60ddcedb98c1638322eb7be3a91d5616becdc
pankajkoree <pankajkori2057@gmail.com>
2025-04-14 22:08:04 +0530

    landing page

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 const Button = ({ text }) => {
   return (
-    <div className="relative w-full md:w-[70%] text-center text-base p-3 font-medium border-2 border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white transition-all duration-300 ease-in-out xl:text-2xl xl:p-6 xl:font-semibold xl:w-auto rounded-xl">
+    <div className="relative w-full md:w-[70%] text-center text-xl p-3 font-medium border-2 border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white transition-all duration-300 ease-in-out xl:text-3xl xl:p-6 xl:font-semibold xl:w-auto rounded-xl">
       {text}
     </div>
   );
